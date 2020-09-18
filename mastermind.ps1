13b80ab7544d0196099fd674969f1922949a2292
GunterPearson <1715@holbertonschool.com>
2020-09-18 14:25:22 -0500

    game is now working

--- mastermind.ps1
+++ mastermind.ps1
@@ -1,24 +1,31 @@
-$available_colors = @('blue', 'green', 'red', 'yellow', 'orange', 'white', 'black')
-$colors = @('red', 'blue', 'green', 'yellow')
+$available_colors = @('blue', 'green', 'red', 'yellow', 'orange', 'black')
+$second_list = @($available_colors + $available_colors + $available_colors + $available_colors)
+$colors = @($second_list | Get-Random -Count 4)
 $numbers = @(0, 1, 2, 3)
 $correct = @('red', 'red', 'red', 'red')
 $counter = 10
+Write-Host "Available colors are: blue, green, red, yellow, orange, black"
 DO
 {
-Write-Host "you have $counter tries left"
-$args = @(((Read-host -Prompt 'Enter comma separated colors').Split(",")).Trim())
+Write-Host "You have $counter tries left, enter 4 comma separated colors"
+$args = @(((Read-host -Prompt '> ').Split(",")).Trim())
 $true_list = @()
 if ($args) {
+    $copy = $colors.Clone()
     ForEach ($i in $numbers) {
-        if ($args[$i] -eq $colors[$i]) {
+        if ($args[$i] -eq $copy[$i]) {
             $true_list += 'red'
-        } elseif ($args[$i] -in $colors) {
-            $true_list += 'white'
+            $copy[$i] = "selected"
         }
+    }
+    ForEach ($i in $numbers) {
+        if ($args[$i] -in $copy) {
+            $true_list += 'white'
+        } 
     }
     $print = Compare-Object $true_list $correct
     if ($null -eq $print) {
-        Write-Host "Congratulations"
+        Write-Host "You are the MASTERMIND!!!!"
         exit
     } else {
         $true_list = $true_list | Sort-Object
@@ -27,3 +34,4 @@ if ($args) {
     }
 }
 } While ( $counter -gt 0)
+Write-Host "The correct answer is $colors"
