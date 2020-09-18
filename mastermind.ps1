3a7595d6e1c5ba02be29aeb038fe25b07f9b70f2
GunterPearson <1715@holbertonschool.com>
2020-09-18 13:22:20 -0500

    pushing updated mastermind file

--- mastermind.ps1
+++ mastermind.ps1
@@ -1,15 +1,29 @@
-$master = @('blue', 'pink', 'red', 'yellow')
-$guess = Read-Host "Please enter your guess"
-$guess = $guess.split()
-Write-Output $guess "this is guess`n"
-Write-Output $master "this is master`n"
-Compare-Object -ReferenceObject $master -DifferenceObject $guess -IncludeEqual | ? {$_.SideINdicator -eq "==" -Or $_.SideINdicator -eq "<="}
-If ($_.SideINdictator -eq "==") {
-   Write-Host "you found matches"
-}
-Else
+$available_colors = @('blue', 'green', 'red', 'yellow', 'orange', 'white', 'black')
+$colors = @('red', 'blue', 'green', 'yellow')
+$numbers = @(0, 1, 2, 3)
+$correct = @('red', 'red', 'red', 'red')
+$counter = 10
+DO
 {
-	Write-Host "guess again"
+Write-Host "you have $counter tries left"
+$args = @(((Read-host -Prompt 'Enter comma separated colors').Split(",")).Trim())
+$true_list = @()
+if ($args) {
+    ForEach ($i in $numbers) {
+        if ($args[$i] -eq $colors[$i]) {
+            $true_list += 'red'
+        } elseif ($args[$i] -in $colors) {
+            $true_list += 'white'
+        }
+    }
+    $print = Compare-Object $true_list $correct
+    if ($null -eq $print) {
+        Write-Host "Congratulations"
+        exit
+    } else {
+        $true_list = $true_list | Sort-Object
+        Write-Host $true_list
+        $counter -= 1
+    }
 }
-$guess = Read-Host "enter guess"
-$guess = $guess.split()
+} While ( $counter -gt 0)
